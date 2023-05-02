eef8e3ad8aee18e83e28b08c32970f87f598bdbf
Jekton Luo <ljtong64@gmail.com>
2023-05-02 15:45:28 +0800

    implement relu

--- src/relu.s
+++ src/relu.s
@@ -13,26 +13,21 @@
 # this function exits with error code 8.
 # ==============================================================================
 relu:
-    # Prologue
-
-
+    ble a1, x0, error
+    li t0, 0
 loop_start:
-    
-
-
-
-
-
-
-
+    slli t1, t0, 2
+    add t1, t1, a0
+    lw t2, 0(t1)
+    bge t2, x0, loop_continue
+    sw x0, 0(t1)
 loop_continue:
-
-
-
+    addi t0, t0, 1
+    blt t0, a1, loop_start
 loop_end:
+    ret
 
-
-    # Epilogue
-
-    
-	ret
+error:
+    li a0, 10
+    li a1, 8
+    ecall
