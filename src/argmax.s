4fc005027ddf18eb9a68f4c998357ce4f54bb8fb
Jekton Luo <ljtong64@gmail.com>
2023-05-02 15:56:11 +0800

    implemented argmax

--- src/argmax.s
+++ src/argmax.s
@@ -15,27 +15,27 @@
 # this function exits with error code 7.
 # =================================================================
 argmax:
-
-    # Prologue
-
+    ble a1, x0, error
+    li t0, 1 # iteration index
+    li t1, 0 # largest index
+    lw t2, 0(a0) # largest value
 
 loop_start:
-
-
-
-
-
-
-
-
+    bge t0, a1, loop_end
+    slli t3, t0, 2
+    add t3, t3, a0
+    lw t3, 0(t3)
+    blt t3, t2, loop_continue
+    mv t1, t0
+    mv t2, t3
 loop_continue:
-
-
-
+    addi t0, t0, 1
+    j loop_start
 loop_end:
-    
-
-    # Epilogue
-
-
+    mv a0, t1
     ret
+
+error:
+    li a0, 10
+    li a1, 7
+    ecall
