64756ef38a3bc5fc4c671208f3aaea510a110f44
Jekton Luo <ljtong64@gmail.com>
2023-05-03 16:29:40 +0800

    implemented read_matrix

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -26,16 +26,84 @@
 read_matrix:
 
     # Prologue
-	
+    addi sp, sp, -36
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    sw s4, 16(sp)
+    sw s5, 20(sp)
+    sw s6, 24(sp)
+    sw s7, 28(sp)
+    sw ra, 32(sp)
+
+    mv s1, a1 # &rows
+    mv s2, a2 # &cols
 
 
 
+    mv a1, a0
+    mv a2, zero
+    call fopen
+    blt a0, zero, fopen_failed
+    mv s0, a0 # fd
 
+    mv a1, s0
+    addi sp, sp, -8
+    mv a2, sp
+    li a3, 8
+    call fread
+    li a1, 8
+    bne a0, a1, fread_failed
 
+    lw s4, 0(sp) # rows
+    lw s5, 4(sp) # cols
+    addi sp, sp, 8
 
+    mul s6, s4, s5
+    slli s6, s6, 2 # size of matrix data
+    mv a0, s6
+    call malloc
+    beq a0, zero, malloc_failed
+    mv s7, a0 # &matrix data
 
+    mv a1, s0
+    mv a2, s7
+    mv a3, s6
+    call fread
+    bne a0, s6, fread_failed
+
+    mv a1, s0
+    call fclose
+    bne a0, zero, fclose_failed
+
+    mv a0, s7
+    sw s4, 0(s1)
+    sw s5, 0(s2)
 
     # Epilogue
-
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw s5, 20(sp)
+    lw s6, 24(sp)
+    lw s7, 28(sp)
+    lw ra, 32(sp)
+    addi sp, sp, 36
 
     ret
+
+malloc_failed:
+    li a1, 48
+    call exit
+fopen_failed:
+    li a1, 50
+    call exit
+fread_failed:
+    li a1, 51
+    call exit
+fclose_failed:
+    li a1, 52
+    call exit
--- tests/assembly/test_read_matrix.s
+++ tests/assembly/test_read_matrix.s
@@ -7,11 +7,18 @@ file_path: .asciiz "inputs/test_read_matrix/test_input.bin"
 .text
 main:
     # Read matrix into memory
-
-
+    la a0, file_path
+    addi sp, sp, -8
+    mv a1, sp
+    addi a2, sp, 4
+    call read_matrix
 
     # Print out elements of matrix
-
-
+    lw a1, 0(sp)
+    lw a2, 4(sp)
+    addi sp, sp, 8
+    call print_int_array
 
     # Terminate the program
+    li a1, 0
+    call exit
